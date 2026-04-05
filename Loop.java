579a7cce494bee96ccc70bc4932764be39ee314f
RITHIKA SRI B <rithikasri2004@gmail.com>
2026-04-05 11:23:31 +0530

    Implemented Java program to display even numbers between 1 and 20

---
+++ Loop.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Loop {
+	public static void main(String[] args) {
+		
+		for(int i =1 ;i % 2==0;i++) {
+			System.out.println("The even Number from 1 to 20 is: "+i);
+			
+		}
+	}
+
+}
