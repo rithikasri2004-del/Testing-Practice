cf311f4c688655a8b15e9e34c022f5fbb021408d
RITHIKA SRI B <rithikasri2004@gmail.com>
2026-04-05 11:19:47 +0530

    Implemented factorial program using Java

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+package practice;
+import java.util.Scanner;
+public class Factorial {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 System.out.println("Enter the number to get the factorial:");
+	        int n = sc.nextInt();
+	        int fact =1;
+	        for(int i=1;i<=n;i++)
+	        {
+	        	fact= fact*i;
+	        }
+	        System.out.println("The factorial of number is : "+fact);
+	        sc.close();
+	        
+	        
+	}
+
+}
